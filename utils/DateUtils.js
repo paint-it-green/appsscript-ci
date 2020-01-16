485e22fffe179fd64399a8669d057d3b03a8ea15
pain-it-green <gerwin@hyperstacksinc.com>
2020-01-16 11:57:50 +0800

    codecov setup

--- utils/DateUtils.js
+++ utils/DateUtils.js
@@ -1,15 +1,16 @@
 var DateUtils = {
-    /*
-    * Converts Date object to a String containing the date part (with dashes).
-    *
-    * @return {String} Date part. E.g. '2018-07-10'.
-    */
-   getDatePart: function(dateObject) {
-      return dateObject.toISOString().slice(0, 10);
-    }
-  }
+  /*
+  * Converts Date object to a String containing the date part (with dashes).
+  *
+  * @return {String} Date part. E.g. '2018-07-10'.
+  */
 
-  if (typeof(exports) !== "undefined") {
-    exports['__esModule'] = true;
-    exports['default'] = DateUtils;
+  getDatePart: function(dateObject) {
+    return dateObject.toISOString().slice(0, 10);
   }
+};
+
+if (typeof(exports) !== 'undefined') {
+  exports['__esModule'] = true;
+  exports['default'] = DateUtils;
+}
