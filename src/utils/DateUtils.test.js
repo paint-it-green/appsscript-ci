c762579ffb1d46d6d7048ec15d12a5531b9f4f20
pain-it-green <gerwin@hyperstacksinc.com>
2020-01-16 17:32:16 +0800

    fix testing test error

--- src/utils/DateUtils.test.js
+++ src/utils/DateUtils.test.js
@@ -3,5 +3,5 @@ import DateUtils from './DateUtils';
 test('getDatePart', () => {
   const date = new Date('2018-07-09');
 
-  expect(DateUtils.getDatePart(date)).toBe('2018-07-0912');
+  expect(DateUtils.getDatePart(date)).toBe('2018-07-09');
 });
